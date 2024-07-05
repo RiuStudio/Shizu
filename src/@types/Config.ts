5725f55523f5f976b6fde454dc8395719e0a0787
Riu <echo.adinfauzan@gmail.com>
2024-07-05 12:17:09 +0000

    Update

--- src/@types/Config.ts
+++ src/@types/Config.ts
@@ -1,38 +1,44 @@
+import { RainlinkNodeOptions } from "../rainlink/main.js";
+
 export interface Config {
   bot: Bot;
-  lavalink: Lavalink;
-  features: Features;
+  player: Player;
+  utilities: Utilities;
+  emojis: Emojis;
 }
 
 export interface Bot {
-  TOKEN: string;
+  TOKEN: string[];
   EMBED_COLOR: string;
   OWNER_ID: string;
+  ADMIN: string[];
   LANGUAGE: string;
-  LIMIT_TRACK: number;
-  LIMIT_PLAYLIST: number;
-  SAFE_ICONS_MODE: boolean;
-  SAFE_PLAYER_MODE: boolean;
-  DELETE_MSG_TIMEOUT: number;
   DEBUG_MODE: boolean;
 }
 
-export interface Features {
+export interface Utilities {
+  AUTO_RESUME: boolean;
+  DELETE_MSG_TIMEOUT: number;
   DATABASE: Database;
+  TOPGG_TOKEN: string;
   MESSAGE_CONTENT: MessageContent;
   AUTOFIX_LAVALINK: AutofixLavalink;
   WEB_SERVER: WebServer;
+  PREMIUM_LOG_CHANNEL: string;
+  GUILD_LOG_CHANNEL: string;
+  LOG_CHANNEL: string;
 }
 
 export interface AutofixLavalink {
   enable: boolean;
-  reconnectTries: number;
-  restTimeout: number;
+  retryCount: number;
+  retryTimeout: number;
 }
 
 export interface Database {
   driver: string;
   config: any;
+  cacheCleanSchedule: string;
 }
 
 export interface MessageContent {
@@ -48,35 +54,48 @@ export interface Commands {
 export interface WebServer {
   enable: boolean;
   port: number;
-  websocket: Websocket;
+  auth: string;
+  whitelist: string[];
 }
 
-export interface Websocket {
-  enable: boolean;
-  host: string;
-  secret: string;
-  auth: boolean;
-  trusted: string[];
-}
-
-export interface Lavalink {
+export interface Player {
   SPOTIFY: Spotify;
   AUTOCOMPLETE_SEARCH: string[];
   NP_REALTIME: boolean;
   LEAVE_TIMEOUT: number;
-  NODES: Node[];
+  NODES: RainlinkNodeOptions[];
   DEFAULT_VOLUME: number;
-}
-
-export interface Node {
-  url: string;
-  name: string;
-  auth: string;
-  secure: boolean;
+  AVOID_SUSPEND: boolean;
+  LIMIT_TRACK: number;
+  LIMIT_PLAYLIST: number;
 }
 
 export interface Spotify {
   enable: boolean;
   id: string;
   secret: string;
+}
+
+export interface Emojis {
+  PLAYER: PlayerEmojis;
+  GLOBAL: GlobalEmojis;
+}
+
+export interface PlayerEmojis {
+  play: string;
+  pause: string;
+  loop: string;
+  shuffle: string;
+  stop: string;
+  skip: string;
+  previous: string;
+  voldown: string;
+  volup: string;
+  queue: string;
+  delete: string;
+}
+
+export interface GlobalEmojis {
+  arrow_next: string;
+  arrow_previous: string;
 }
